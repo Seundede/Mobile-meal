394f164121bee5d021b2a41153e04b141cbd008d
Seundede <omadoyedede0@gmail.com>
2022-07-09 03:08:01 +0100

    sanity

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { StarIcon } from "react-native-heroicons/solid";
+import { LocationMarkerIcon } from "react-native-heroicons/outline";
+
 
 export default function RestaurantCard({
-    id,imageUrl,title,rating,genre,address,shortDescription,dishes,longitude,latitude
+  id,
+  imageUrl,
+  title,
+  rating,
+  genre,
+  address,
+  shortDescription,
+  dishes,
+  longitude,
+  latitude,
 }) {
   return (
-    <View>
-      <Text>RestaurantCard</Text>
-    </View>
-  )
+    <TouchableOpacity style={tw`bg-white shadow mr-4`}>
+      <Image
+        source={{
+          uri: imageUrl,
+        }}
+        style={tw`h-36 w-64 rounded-sm`}
+      />
+      <View style={tw`px-3 pb-4`}>
+        <Text style={tw`font-bold text-lg pt-2`}>{title}</Text>
+        <View style={tw`flex-row items-center`}>
+          <StarIcon color="green" />
+          <Text style={tw`text-xs text-gray-500 ml-1`}>
+            <Text style={tw`text-green-600`}>{rating}</Text>. {genre}
+          </Text>
+        </View>
+        <View style={tw`flex-row items-center`}>
+          <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+          <Text style={tw`text-xs text-gray-500 ml-1`}> Nearby . {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 }
